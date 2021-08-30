108430c01ac5a91fbebcd3f952ceedd682d07522
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-08-30 19:21:15 +0200

    Create UploadedFileValidationExtension.php

---
+++ src/validation/UploadedFileValidationExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bermuda\Files\Validation;
+
+use Psr\Http\Message\UploadedFileInterface;
+
+final class UploadedFileValidationExtension extends \RuntimeException
+{
+    public function __construct(private array $errors,
+                                private UploadedFileInterface $uploadedFile
+    )
+    {
+        parent::__construct('Uploaded file validation is failed', 400);
+    }
+
+    /**
+     * @return UploadedFileInterface
+     */
+    public function getUploadedFile(): UploadedFileInterface
+    {
+        return $this->uploadedFile;
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
