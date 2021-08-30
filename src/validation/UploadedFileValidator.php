11efc69b09982a7c1812bc7a0a9a9a84930be0fc
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-08-30 19:18:38 +0200

    Create UploadedFileValidator.php

---
+++ src/validation/UploadedFileValidator.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Bermuda\Files\Validation;
+
+use Bermuda\Files\FileInfo;
+use Psr\Http\Message\UploadedFileInterface;
+
+final class UploadedFileValidator implements UploadedFileValidatorInterface
+{
+    public const mimeType  = 'mimeType';
+    public const fileSize  = 'fileSize';
+    public const extension = 'extension';
+
+    public const MAX_FILESIZE_1MB = 8e+6;
+    public const MAX_FILESIZE_3MB = 2.4e+7;
+    public const MAX_FILESIZE_5MB = 4e+7;
+    public const MAX_FILESIZE_10MB = 8e+7;
+    public const MAX_FILESIZE_15MB = 1.2e+8;
+    public const MAX_FILESIZE_20MB = 1.6e+8;
+    public const MAX_FILESIZE_25MB = 2e+8;
+
+    /**
+     * @param array $config
+     * @return UploadedFileValidator
+     */
+    public static function instantiate(array $config): self
+    {
+        return new self(
+            $config[self::mimeType] ?? [],
+            $config[self::extension] ?? [],
+            $config[self::fileSize] ?? null,
+        );
+    }
+
+    public function __construct(
+        private string|array $mimeType,
+        private string|array $extension,
+        private ?int          $filesize,
+    )
+    {
+        $this->mimeType = is_array($mimeType) ? $mimeType : [$mimeType];
+        $this->extension = is_array($extension) ? $extension : [$extension];
+    }
+
+    /**
+     * @param UploadedFileInterface $file
+     */
+    public function validate(UploadedFileInterface $file): void
+    {
+        $uri = $file->getStream()->getMetadata('uri');
+
+        $errors = [];
+
+        if ($this->mimeType != [] && !in_array($mimeType = FileInfo::mimeType($uri), $this->mimeType))
+        {
+            $errors[] = sprintf('Invalid mimeType: %s, allowed types: %s; File: %s',
+                $mimeType, implode(',', $this->mimeType), $file->getClientFilename()
+            );
+        }
+
+        if ($this->extension != [] && in_array($ext = FileInfo::extension($uri, true), $this->extension))
+        {
+            $errors[] = sprintf('Invalid file extension: %s, allowed extensions: %s; File: %s',
+                $ext, implode(',', $this->extension), $file->getClientFilename()
+            );
+        }
+
+        if ($this->filesize != null && FileInfo::filesize($uri) > $this->filesize)
+        {
+            $errors[] = 'Maximum file size exceeded; File: ' . $file->getClientFilename();
+        }
+
+        $errors === [] ?: throw new UploadedFileValidationExtension($errors, $file);
+    }
+}
