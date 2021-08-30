4e2e9be68d0cac0e81027d0156c2989449cdb471
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-08-30 18:57:01 +0200

    Create FileSystemFactory.php

---
+++ src/FileSystemFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bermuda\Files;
+
+use League\Flysystem\Filesystem;
+use League\Flysystem\Local\LocalFilesystemAdapter;
+
+final class FileSystemFactory
+{
+    public function __invoke(): Filesystem
+    {
+        return self::makeSystem();
+    }
+
+    /**
+     * @param string|null $path
+     * @return Filesystem
+     */
+    public static function makeSystem(string $path = null): Filesystem
+    {
+        return new Filesystem(new LocalFilesystemAdapter($path ?? getcwd()));
+    }
+}
