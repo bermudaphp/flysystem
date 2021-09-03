d53bada9dc62adae46e913c8f1489849339f103f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-03 18:13:38 +0200

    Create ConfigProvider.php

---
+++ src/ConfigProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bermuda\Flysystem;
+
+final class ConfigProvider extends \Bermuda\Config\ConfigProvider
+{
+    /**
+     * @inheritDoc
+     */
+    protected function getFactories(): array
+    {
+        return [
+            Flysystem::class => 'Bermuda\Flysyste\Flysystem::makeLocal',
+            FileProcessorInterface::class => 'Bermuda\Flysystem\UploadedFilesHandler::fromContainer'
+        ];
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    protected function getAliases(): array
+    {
+        return [UploadedFilesHandler::class => FileProcessorInterface::class];
+    }
+}
