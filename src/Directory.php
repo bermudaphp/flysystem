0eae30e3734cb18ef8b59669f8ff96466013c857
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-11 20:16:16 +0200

    Update Directory.php

--- src/Directory.php
+++ src/Directory.php
@@ -2,31 +2,12 @@
 
 namespace Bermuda\Flysystem;
 
-use League\Flysystem\FilesystemOperator;
-use Psr\Http\Message\StreamFactoryInterface;
+use Countable;
+use Generator;
+use League\Flysystem\FilesystemException;
 
-final class Directory extends FlysystemData implements \Countable
+final class Directory extends FlysystemData implements Countable
 {
-    /**
-     * @param string $location
-     * @param FilesystemOperator|null $system
-     * @param StreamFactoryInterface|null $streamFactory
-     * @return static
-     * @throws \League\Flysystem\FilesystemException
-     * @throws Exceptions\NoSuchDirectory
-     */
-    public static function open(
-        string $location, ?Flysystem $system = null
-    ): self
-    {
-        if (!($system = self::system($system))->isDirectory($location))
-        {
-           throw new Exceptions\NoSuchDirectory($location);
-        }
-
-        return new self($location, $system);
-    }
-
     /**
      * @return string
      */
@@ -36,44 +17,21 @@ final class Directory extends FlysystemData implements \Countable
     }
 
     /**
-     * @param string $location
-     * @param FilesystemOperator|null $system
-     * @param StreamFactoryInterface|null $streamFactory
-     * @return static
-     * @throws \League\Flysystem\FilesystemException
-     */
-    public static function create(string $location, ?Flysystem $system = null
-    ): self
-    {
-        try {
-            return self::open($location, $system);
-        } catch (Exceptions\NoSuchDirectory $e) {
-            ($system = self::system($system))->getOperator()
-                ->createDirectory($location);
-            
-            return self::open($location, $system);
-        }
-    }
-
-    /**
      * @param self[] $directories
      * @param bool $deleteMerged
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function merge(array $directories, bool $deleteMerged = false): void
     {
-        foreach ($directories as $directory)
-        {
+        foreach ($directories as $directory) {
             /**
              * @var File|self $flysystemData
              */
-            foreach ($directory as $flysystemData)
-            {
+            foreach ($directory as $flysystemData) {
                 $flysystemData->copy($this->location, true);
             }
 
-            if ($deleteMerged)
-            {
+            if ($deleteMerged) {
                 $directory->delete();
             }
         }
@@ -82,18 +40,13 @@ final class Directory extends FlysystemData implements \Countable
     /**
      * @param string $destination
      * @return Directory
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     final public function copy(string $destination): self
     {
-        $destination = $this->normalizePath($destination);
-
         $directory = self::create($destination, $this->flysystem);
-
-        foreach ($this as $flysystemData)
-        {
-            if ($directory->getName() !== $flysystemData->getName())
-            {
+        foreach ($this as $flysystemData) {
+            if ($directory->getName() !== $flysystemData->getName()) {
                 $directory->add($flysystemData);
             }
         }
@@ -102,14 +55,71 @@ final class Directory extends FlysystemData implements \Countable
     }
 
     /**
+     * @param string $location
+     * @param Flysystem|null $system
+     * @return self
+     * @throws FilesystemException
+     */
+    public static function create(string $location, ?Flysystem $system = null
+    ): self
+    {
+        try {
+            return self::open($location, $system);
+        } catch (Exceptions\NoSuchDirectory) {
+            ($system = self::system($system))->getOperator()
+                ->createDirectory($location);
+            return self::open($location, $system);
+        }
+    }
+
+    /**
+     * @param string $location
+     * @param Flysystem|null $system
+     * @return self
+     * @throws Exceptions\NoSuchDirectory
+     */
+    public static function open(
+        string $location, ?Flysystem $system = null
+    ): self
+    {
+        if (!($system = self::system($system))->isDirectory($location)) {
+            throw new Exceptions\NoSuchDirectory($location);
+        }
+
+        return new self($location, $system);
+    }
+
+    /**
+     * @param File|Directory $flysystemData
+     * @throws FilesystemException
+     */
+    public function add(File|self $flysystemData): void
+    {
+        if ($flysystemData instanceof self) {
+            $flysystemData->copy($this->location->append($flysystemData->getName()));
+        } else {
+            $flysystemData->copy($this->location, true);
+        }
+    }
+
+    /**
+     * @throws FilesystemException
+     */
+    public function delete(): void
+    {
+        $this->flysystem->getOperator()->deleteDirectory($this->location);
+    }
+
+    // public function isRoot(): bool{}
+
+    /**
      * @return int
      */
     public function getSize(): int
     {
         $size = 0;
 
-        foreach ($this as $item)
-        {
+        foreach ($this as $item) {
             $size += $item->getSize();
         }
 
@@ -119,23 +129,20 @@ final class Directory extends FlysystemData implements \Countable
     /**
      * @param string $path
      * @return bool
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function exists(string $path): bool
     {
         return $this->flysystem->exists($this->location->append($path));
     }
 
-    // public function isRoot(): bool{}
-
     /**
      * @return bool
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function isEmpty(): bool
     {
-        foreach ($this->flysystem->getOperator()->listContents($this->location) as $i)
-        {
+        foreach ($this->flysystem->getOperator()->listContents($this->location) as $i) {
             return false;
         }
 
@@ -144,11 +151,31 @@ final class Directory extends FlysystemData implements \Countable
 
     /**
      * @return bool
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function hasChildes(): bool
     {
         return $this->getChildes() !== [];
+    }
+
+    /**
+     * @return self[]
+     * @throws FilesystemException
+     */
+    public function getChildes(): array
+    {
+        return $this->listContents('/', static fn(FlysystemData $v) => $v instanceof self);
+    }
+
+    /**
+     * @param string $path
+     * @param callable|null $filter
+     * @return array
+     * @throws FilesystemException
+     */
+    public function listContents(string $path = '/', callable $filter = null): array
+    {
+        return $this->flysystem->listContents($this->location->append($path), $filter);
     }
 
     /**
@@ -159,44 +186,21 @@ final class Directory extends FlysystemData implements \Countable
     {
         $count = 0;
 
-        foreach ($this as $item)
-        {
+        foreach ($this as $item) {
             $count += $recursive ? $item->count() + 1 : 1;
         }
 
         return $count;
     }
 
-    public function getIterator(): \Generator
+    /**
+     * @return Generator
+     * @throws FilesystemException
+     */
+    public function getIterator(): Generator
     {
-        foreach ($this->listContents() as $flysystemData)
-        {
+        foreach ($this->listContents() as $flysystemData) {
             yield $flysystemData;
-        }
-    }
-
-    /**
-     * @throws \League\Flysystem\FilesystemException
-     */
-    public function delete(): void
-    {
-        $this->flysystem->getOperator()->deleteDirectory($this->location);
-    }
-
-    /**
-     * @param File|Directory $flysystemData
-     * @throws \League\Flysystem\FilesystemException
-     */
-    public function add(File|self $flysystemData): void
-    {
-        if ($flysystemData instanceof self)
-        {
-            $flysystemData->copy($this->location . self::separator . $flysystemData->getName());
-        }
-
-        else
-        {
-            $flysystemData->copy($this->location, true);
         }
     }
 
@@ -204,17 +208,17 @@ final class Directory extends FlysystemData implements \Countable
      * @param string $filename
      * @param string $content
      * @return File
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function addNewFile(string $filename, string $content): File
     {
-        return File::create($this->location->append($filename), $content, $this->system, $this->streamFactory);
+        return File::create($this->location->append($filename), $content, $this->flysystem);
     }
 
     /**
      * @param string $location
      * @return $this
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function addNewDirectory(string $location): self
     {
@@ -222,7 +226,8 @@ final class Directory extends FlysystemData implements \Countable
     }
 
     /**
-     * @return File[]
+     * @return array
+     * @throws FilesystemException
      */
     public function getFiles(): array
     {
@@ -230,28 +235,8 @@ final class Directory extends FlysystemData implements \Countable
     }
 
     /**
-     * @return self[]
-     * @throws \League\Flysystem\FilesystemException
-     */
-    public function getChildes(): array
-    {
-        return $this->listContents('/', static fn($v) => $v instanceof self);
-    }
-
-    /**
-     * @param string $path
-     * @param callable|null $filter
      * @return array
-     * @throws \League\Flysystem\FilesystemException
-     */
-    public function listContents(string $path = '/', callable $filter = null): array
-    {
-        return $this->flysystem->listContents($this->location . self::separator . $path, $filter);
-    }
-
-    /**
-     * @return array
-     * @throws \League\Flysystem\FilesystemException
+     * @throws FilesystemException
      */
     public function toArray(): array
     {
