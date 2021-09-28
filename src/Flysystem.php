fe0a00f3d9470949fd1f706106eb779d324f4ea7
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-28 12:50:35 +0200

    Update Flysystem.php

--- src/Flysystem.php
+++ src/Flysystem.php
@@ -17,6 +17,7 @@ use League\Flysystem\{FilesystemOperator, FilesystemException};
  */
 final class Flysystem
 {
+    use Iterate;
     public function __construct(private ?FilesystemOperator     $operator = null,
                                 private ?StreamFactoryInterface $streamFactory = null,
                                 private ?ExtensionDetector      $detector = null
