aedc07dac94a8f3f2196af035919cedfd4c721ab
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-01 17:11:04 +0200

    Update NoSuchFile.php

--- src/NoSuchFile.php
+++ src/NoSuchFile.php
@@ -6,6 +6,6 @@ final class NoSuchFile extends \RuntimeException
 {
     public function __construct(string $filename)
     {
-        parent::__construct('No such file or directory: ' . $filename);
+        parent::__construct('No such file or directory: ' .$filename);
     }
 }
