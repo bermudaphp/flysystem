443a9efdab66ebedc2b935d41706c25ceba7857b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-14 15:29:11 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -2,8 +2,11 @@
 
 namespace Bermuda\Flysystem;
 
+use Bermuda\Detector\FinfoDetector;
+use League\Flysystem\Filesystem;
 use League\Flysystem\FilesystemOperator;
 use Bermuda\Detector\ExtensionDetector;
+use League\Flysystem\Local\LocalFilesystemAdapter;
 use function Bermuda\Config\cget;
 
 final class ConfigProvider extends \Bermuda\Config\ConfigProvider
@@ -15,7 +18,8 @@ final class ConfigProvider extends \Bermuda\Config\ConfigProvider
     {
         return [
             Flysystem::class => static fn(ContainerInterface $container) => new Flysystem(
-                cget($container, FilesystemOperator::class), cget($container, ExtensionDetector::class)
+                cget($container, FilesystemOperator::class, static fn() => new Filesystem(new LocalFilesystemAdapter(getcwd())), true), 
+                cget($container, ExtensionDetector::class, static fn() => new FinfoDetector(), true)
             )
         ];
     }
